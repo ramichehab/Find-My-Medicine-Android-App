41fc0058d8fe11fda9f20e6f0946ffe7a02bbeae
ramichehab <64501890+ramichehab@users.noreply.github.com>
2021-12-04 22:51:59 +0200

    update 5

--- app/src/main/java/com/lau/findmymedicine2/Medicine.java
+++ app/src/main/java/com/lau/findmymedicine2/Medicine.java
@@ -4,11 +4,39 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Medicine extends AppCompatActivity {
+public class Medicine{
+    String Name;
+    String Use;
+    String Warning;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_medicine);
+    Medicine(String Name, String Use, String Warning){
+        this.Name=Name;
+        this.Use=Use;
+        this.Warning=Warning;
+
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public String getUse() {
+        return Use;
+    }
+
+    public String getWarning() {
+        return Warning;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public void setUse(String use) {
+        Use = use;
+    }
+
+    public void setWarning(String warning) {
+        Warning = warning;
     }
 }
--- app/src/main/java/com/lau/findmymedicine2/Search.java
+++ app/src/main/java/com/lau/findmymedicine2/Search.java
@@ -1,49 +1,123 @@
 package com.lau.findmymedicine2;
 
 import static android.content.ContentValues.TAG;
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.protobuf.StringValue;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class Search extends AppCompatActivity {
 
     FirebaseFirestore db = FirebaseFirestore.getInstance();
+    DocumentReference docRef;
+    int i;
+    ArrayList<String> medName= new ArrayList<String>();
+
+    ArrayAdapter arrayAdapter;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
-        db.collection("medicines")
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                Log.i("test", document.getId() + " => " + document.getData());
-                            }
+
+
+//        db.collection("medicines")
+//                .get()
+//                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+//                        if (task.isSuccessful()) {
+//                            for (QueryDocumentSnapshot document : task.getResult()) {
+//                                //medName.add(document.getString("Name"));
+//                                Log.i("test1",document.getString("Name"));
+//                            }
+//                        } else {
+//                            Log.d(TAG, "Error getting documents: ", task.getException());
+//                        }
+//                    }
+//                });
+
+
+
+
+
+            docRef = db.collection("medicines").document("1");
+            docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                    if (task.isSuccessful()) {
+                        DocumentSnapshot document = task.getResult();
+                        if (document.exists()) {
+                            Log.i("test1",document.getString("Name"));
+
+
+
+
+
+
+
+
+
                         } else {
-                            Log.w(TAG, "Error getting documents.", task.getException());
+                            Log.d(TAG, "No such document");
                         }
+                    } else {
+                        Log.d(TAG, "get failed with ", task.getException());
                     }
-                });
+                }
+            });
+
+        }
+//
+//
+//        }
+//            ListView listView = (ListView) findViewById(R.id.ls2);
+//
+//            arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, medName);
+//
+//
+//            listView.setAdapter(arrayAdapter);
+
+//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int i, long l) {
+//                Intent intent =new Intent(getApplicationContext(),.class);
+//                intent.putExtra("content",content.get(i));
+//                startActivity(intent);
+//            }
+//        });
+
+
 
     }
 
 
-}
